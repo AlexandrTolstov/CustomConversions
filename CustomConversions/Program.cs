c380ae6ae26c2eae0405be5c587b28b495793131
Толстов Александр <alextav_85@mail.ru>
2019-07-26 09:11:31 +0600

    Add project files.

---
+++ CustomConversions/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomConversions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("****** Fun with Conversion ******\n");
+            Rectangle r = new Rectangle(15, 4);
+            Console.WriteLine(r.ToString());
+            r.Draw();
+
+            Console.WriteLine();
+            Square s = (Square)r;
+            Console.WriteLine(s.ToString());
+            s.Draw();
+            Console.ReadLine();
+        }
+    }
+    public struct Rectangle
+    {
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public Rectangle(int w, int h) : this()
+        {
+            Width = w;
+            Height = h;
+        }
+        public void Draw()
+        {
+            for (int i = 0; i < Height; i++)
+            {
+                for (int j = 0; j < Width; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+        public override string ToString()
+        {
+            return $"[Width = {Width}; Height = {Height}]";
+        }
+    }
+    public struct Square
+    {
+        public int Lenght { get; set; }
+        public Square(int l) : this()
+        {
+            Lenght = 1;
+        }
+        public void Draw()
+        {
+            for (int i = 0; i < Lenght; i++)
+            {
+                for (int j = 0; j < Lenght; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+        public override string ToString()
+        {
+            return $"[Length = {Lenght}]";
+        }
+        public static explicit operator Square(Rectangle r)
+        {
+            Square s = new Square { Lenght = r.Height };
+            return s;
+        }
+    }
+}
